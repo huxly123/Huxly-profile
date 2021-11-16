126daa58b5a2b526f475024e98e891f627c41182
huxly123 <huxlygerard5@gmail.com>
2021-11-16 17:46:03 +0530

    profile

---
+++ profile/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from "./navbar.module.css"
+
+function Navbar() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navbar
