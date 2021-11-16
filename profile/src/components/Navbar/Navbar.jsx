7ad0925f96e26962a3b518cc38a523dffd3294c2
huxly123 <huxlygerard5@gmail.com>
2021-11-17 00:47:15 +0530

    profile

--- profile/src/components/Navbar/Navbar.jsx
+++ profile/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import style from "./navbar.module.css"
+import React from "react";
+import style from "./navbar.module.css";
 
 function Navbar() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className={style.navbody}>
+      <div className={style.hs}>HS</div>
+      <div className={style.flexx}>
+        <a>About</a>
+        <a>Skills</a>
+        <a>Project</a>
+        <a>Blogs</a>
+         <a>Contact</a>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
