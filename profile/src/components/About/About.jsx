964b147c3123ac5ab1cafd259b7c4b6405dc6ed0
huxly123 <huxlygerard5@gmail.com>
2021-11-21 15:07:55 +0530

    fff

--- profile/src/components/About/About.jsx
+++ profile/src/components/About/About.jsx
@@ -15,23 +15,7 @@ function About() {
 
       <div className={style.aboutflex} style={{ marginTop: "10px" }}>
         <div style={{ position: "relative" }}>
-          <svg
-            style={{ left: "-30px", position: "absolute" }}
-            stroke="currentColor"
-            fill="currentColor"
-            stroke-width="0"
-            viewBox="0 0 20 20"
-            class="About_icon__1B8FN"
-            height="1.3em"
-            width="1.3em"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
+          <img src="/pin.gif" style={{width:"1.3em",height:"1.3em",position:"absolute",left:"-30px"} }/>
           <span>Chennai,TamilNadu</span>
         </div>
 
--- profile/src/components/Navbar/Navbar.jsx
+++ profile/src/components/Navbar/Navbar.jsx
@@ -6,11 +6,11 @@ function Navbar() {
     <div className={style.navbody}>
       <div className={style.hs}>HS</div>
       <div className={style.flexx}>
-        <a>About</a>
-        <a>Skills</a>
-        <a>Project</a>
-        <a>Blogs</a>
-         <a>Contact</a>
+        <a className={style.a1}>About</a>
+        <a className={style.a2}>Skills</a>
+        <a className={style.a3}>Project</a>
+        <a className={style.a4}>Blogs</a>
+        <a className={style.a5}>Contact</a>
       </div>
     </div>
   );
