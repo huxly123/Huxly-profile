90a4b685a1e7fe309193f9d66bab99fa2aa226df
huxly123 <huxlygerard5@gmail.com>
2022-01-06 01:10:26 +0530

    project

--- profile/src/components/Projects/Project.jsx
+++ profile/src/components/Projects/Project.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Project1 from "./Project-1/Project1";
 import Project2 from "./Project-2/Project2";
 import Project3 from "./Project-3/Project3";
+import Project4 from "./Project-4/Project4";
 import style from "./project.module.css";
 
 function Project() {
@@ -16,9 +17,9 @@ function Project() {
       <div className={style.project}>
         <div className={style.projectcontent}>
           <Project3 />
-          <Project2 />
-
+          <Project4 />
           <Project1 />
+          <Project2 />
         </div>
       </div>
     </>
